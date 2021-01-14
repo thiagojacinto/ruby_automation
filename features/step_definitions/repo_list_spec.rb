348d1519a6f85792684bb94b5aa93ffba1929992
thiagojacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-01-14 18:11:09 -0300

    feat: step de asserção da adição bem sucedida

--- features/step_definitions/repo_list_spec.rb
+++ features/step_definitions/repo_list_spec.rb
@@ -5,10 +5,8 @@ end
 When('add the repository {string} from {string}') do |repository_name, user|
   find('input[name=repository__input]').set "#{user}/#{repository_name}"
   click_button('Add')
-
-  sleep 5
 end
 
 Then('{string} should appear on the repository list') do |repository_name|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(find('main ul')).to have_content repository_name
 end
