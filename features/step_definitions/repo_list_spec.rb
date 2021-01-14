7a99d2d799f60352865f3be33e5fcd26d19feecd
Thiago Jacinto <thiagojs.santos@gmail.com>
2021-01-14 13:20:24 -0300

    feat: steps para feature > repo_list

--- features/step_definitions/repo_list_spec.rb
+++ features/step_definitions/repo_list_spec.rb
@@ -1,7 +1,14 @@
-When('add the repository {string} from {string}') do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('access home') do
+  visit "https://repo-listing.web.app/"
 end
 
-Then('{string} should appear on the repository list') do |string|
+When('add the repository {string} from {string}') do |repository_name, user|
+  find('input[name=repository__input]').set "#{user}/#{repository_name}"
+  click_button('Add')
+
+  sleep 5
+end
+
+Then('{string} should appear on the repository list') do |repository_name|
   pending # Write code here that turns the phrase above into concrete actions
 end
